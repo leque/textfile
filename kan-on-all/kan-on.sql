f2840ba9a34bb38da164110bcefc95930ef2a2bf
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2025-12-28 02:48:56 +0900

    常用漢字でうまく変換できないものにある程度対応

--- kan-on-all/kan-on.sql
+++ kan-on-all/kan-on.sql
@@ -9,11 +9,11 @@ SELECT
                    FROM `隋語廣韻全字表.csv` AS z1)
                    UNION ALL
                    (SELECT
-                      os.`字`,
+                      i.`字`,
                       z2.`小韻`
                       FROM `隋語廣韻全字表.csv` AS z2
-                           INNER JOIN `jp-old-style.csv` AS os
-                               ON z2.`字` = os.`旧字`))
+                           INNER JOIN `itaiji.csv` AS i
+                               ON z2.`字` = i.`異体字`))
                  AS z
         ON k.`小韻` = z.`小韻`
  WHERE
